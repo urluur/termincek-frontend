c347e03ad70f67b10ce8fdf2ede5579a1a7a329e
Luka Uršič <luka.urs@icloud.com>
2024-01-14 19:09:59 +0100

    nastavljanje cenika
    - dodajanje storitev
    - urejanje storitev
    - brisanje storitev

--- src/components/admin/Cenik/Cenik.jsx
+++ src/components/admin/Cenik/Cenik.jsx
@@ -1,8 +1,108 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useContext, useState, useEffect, useCallback } from 'react'
+import { API_URL } from '../../../utils/utils'
+import { PodjetjeContext } from '../../../contexts/contexts'
+import { Button, Card, Container, Row, Col, Modal, Alert } from 'react-bootstrap'
+import NovaEditStoritev from '../NovaEditStoritev/NovaEditStoritev'
 
 function Cenik() {
+  const { podjetje } = useContext(PodjetjeContext);
+  const [storitve, setStoritve] = useState([]);
+  const [selectedStoritev, setSelectedStoritev] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+
+  const fetchStoritve = useCallback(() => {
+    axios.get(API_URL + "/storitve/" + podjetje.podjetje_id)
+      .then(response => {
+        setStoritve(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }, [podjetje.podjetje_id]);
+
+  useEffect(() => {
+    fetchStoritve();
+  }, [fetchStoritve]);
+
+  const handleDelete = (id) => {
+    // delete storitev
+    axios.delete(API_URL + "/storitev/" + id, {
+      withCredentials: true,
+      timeout: 20000
+    })
+      .then(response => {
+        setStoritve(storitve.filter(storitev => storitev.storitev_id !== id));
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }
+
+  const handleEdit = (storitev) => {
+    setSelectedStoritev({ ...storitev });
+    setShowModal(true);
+  }
+
+  const handleClose = () => {
+    setShowModal(false);
+    fetchStoritve();
+  }
+
   return (
-    <div>Cenik</div>
+    <Container>
+      <Row className="align-items-center mb-3">
+        <Col>
+          <h1>Cenik</h1>
+        </Col>
+        <Col xs="auto">
+          <Button variant="success" size="lg" onClick={() => handleEdit({})}>Nova storitev</Button>
+        </Col>
+      </Row>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{selectedStoritev && selectedStoritev.storitev_id ? 'Uredi storitev' : 'Nova storitev'}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedStoritev && <NovaEditStoritev storitev={selectedStoritev} mode={selectedStoritev.storitev_id ? 'edit' : 'create'} handleClose={handleClose} />}
+        </Modal.Body>
+      </Modal>
+      <Row>
+        {storitve.length > 0 ? (
+          storitve.map(storitev => (
+            <Col md={6} lg={4} className="mb-3">
+              <Card key={storitev.storitev_id}>
+                <Row className="no-gutters">
+                  <Col md={8}>
+                    <Card.Body>
+                      <Card.Title>{storitev.storitev_ime}</Card.Title>
+                      <Card.Text>Opis: {storitev.storitev_opis}</Card.Text>
+                      <Card.Text>Cena: {storitev.storitev_cena}€</Card.Text>
+                      <Card.Text>Čas trajanja: {storitev.storitev_trajanje} min</Card.Text>
+                      <Button className='m-1' variant="primary" onClick={() => handleEdit(storitev)}>Uredi</Button>
+                      <Button className='m-1' variant="danger" onClick={() => handleDelete(storitev.storitev_id)}>Izbriši</Button>
+                    </Card.Body>
+                  </Col>
+                  <Col md={4}>
+                    <Card.Img src={storitev.storitev_slika} className="img-fluid" />
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+          ))
+        ) : (
+          <Col className="d-flex justify-content-center">
+            <Alert variant="success" className='p-4 d-inline-flex align-items-center flex-column'>
+              <div>
+                <Alert.Heading>Vaše podjetje trenutno ne nudi storitev.</Alert.Heading>
+                <p>Dodajte storitev z gumbom Nova storitev.</p>
+              </div>
+            </Alert>
+          </Col>
+        )}
+      </Row>
+    </Container>
   )
 }
 
---
+++ src/components/admin/NovaEditStoritev/NovaEditStoritev.jsx
@@ -0,0 +1,96 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+import { API_URL } from '../../../utils/utils';
+import { PodjetjeContext } from '../../../contexts/contexts';
+
+function NovaEditStoritev({ storitev, mode, handleClose }) {
+  const { podjetje } = useContext(PodjetjeContext);
+  const [formStoritev, setFormStoritev] = useState(storitev || {
+    storitev_ime: '',
+    storitev_opis: '',
+    storitev_slika: '',
+    storitev_cena: 0,
+    storitev_trajanje: 0,
+    podjetje_id: podjetje.podjetje_id
+  });
+
+  useEffect(() => {
+    setFormStoritev(storitev);
+  }, [storitev]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    let finalValue = value;
+
+    if (name === 'storitev_cena' || name === 'storitev_trajanje') {
+      finalValue = Number(value);
+      if (isNaN(finalValue) || finalValue < 0) {
+        finalValue = 0;
+      }
+    }
+
+    setFormStoritev(prevState => ({
+      ...prevState,
+      [name]: finalValue
+    }));
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let response;
+      if (mode === 'edit') {
+        response = await axios.post(API_URL + "/storitev/uredi/" + formStoritev.storitev_id, formStoritev, {
+          withCredentials: true,
+          timeout: 20000
+        });
+      } else {
+        // set podjetje_id
+        formStoritev.podjetje_id = podjetje.podjetje_id;
+        response = await axios.post(API_URL + "/storitev/nova", formStoritev, {
+          withCredentials: true,
+          timeout: 20000
+        });
+      }
+      console.log(response.data);
+      handleClose(); // Close the modal
+    } catch (error) {
+      console.error('There was an error!', error);
+    }
+  }
+  return (
+    <Form onSubmit={handleSubmit} >
+      <Form.Group controlId="formStoritevIme" className="mb-3">
+        <Form.Label>Ime storitve</Form.Label>
+        <Form.Control type="text" name="storitev_ime" value={formStoritev?.storitev_ime || ''} onChange={handleChange} required className="shadow-none" />
+      </Form.Group>
+
+      <Form.Group controlId="formStoritevOpis" className="mb-3">
+        <Form.Label>Opis storitve</Form.Label>
+        <Form.Control type="text" name="storitev_opis" value={formStoritev?.storitev_opis || ''} onChange={handleChange} required className="shadow-none" />
+      </Form.Group>
+
+      <Form.Group controlId="formStoritevSlika" className="mb-3">
+        <Form.Label>Slika storitve</Form.Label>
+        <Form.Control type="text" name="storitev_slika" value={formStoritev?.storitev_slika || ''} onChange={handleChange} required className="shadow-none" />
+      </Form.Group>
+
+      <Form.Group controlId="formStoritevCena" className="mb-3">
+        <Form.Label>Cena storitve</Form.Label>
+        <Form.Control type="number" name="storitev_cena" value={formStoritev?.storitev_cena} onChange={handleChange} required className="shadow-none" />
+      </Form.Group>
+
+      <Form.Group controlId="formStoritevTrajanje" className="mb-3">
+        <Form.Label>Trajanje storitve</Form.Label>
+        <Form.Control type="number" name="storitev_trajanje" value={formStoritev?.storitev_trajanje} onChange={handleChange} required className="shadow-none" />
+      </Form.Group>
+
+      <Button className='mt-3' variant="primary" type="submit">
+        Potrdi
+      </Button>
+    </Form>
+  )
+}
+
+export default NovaEditStoritev
